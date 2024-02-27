dc43358d20db754ce2238b4e330bc6a6135c1692
tickt <tickt@MRTICKTAKEN>
2024-02-26 22:02:14 -0600

    Agregar archivos de proyecto.

---
+++ IVA/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace IVA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.htmlLabel1Resultado = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.htmlLabel1 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.htmlLabel2 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroButton1Cotiza = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // htmlLabel1Resultado
+            // 
+            this.htmlLabel1Resultado.AutoScroll = true;
+            this.htmlLabel1Resultado.AutoScrollMinSize = new System.Drawing.Size(10, 0);
+            this.htmlLabel1Resultado.AutoSize = false;
+            this.htmlLabel1Resultado.BackColor = System.Drawing.SystemColors.Window;
+            this.htmlLabel1Resultado.Location = new System.Drawing.Point(123, 258);
+            this.htmlLabel1Resultado.Name = "htmlLabel1Resultado";
+            this.htmlLabel1Resultado.Size = new System.Drawing.Size(247, 94);
+            this.htmlLabel1Resultado.TabIndex = 0;
+            // 
+            // htmlLabel1
+            // 
+            this.htmlLabel1.AutoScroll = true;
+            this.htmlLabel1.AutoScrollMinSize = new System.Drawing.Size(198, 35);
+            this.htmlLabel1.AutoSize = false;
+            this.htmlLabel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.htmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.htmlLabel1.Location = new System.Drawing.Point(147, 63);
+            this.htmlLabel1.Name = "htmlLabel1";
+            this.htmlLabel1.Size = new System.Drawing.Size(202, 37);
+            this.htmlLabel1.TabIndex = 1;
+            this.htmlLabel1.Text = "Precio del Producto";
+            this.htmlLabel1.Click += new System.EventHandler(this.htmlLabel1_Click);
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(115, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(147, 116);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(137, 23);
+            this.metroTextBox1.TabIndex = 2;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // htmlLabel2
+            // 
+            this.htmlLabel2.AutoScroll = true;
+            this.htmlLabel2.AutoScrollMinSize = new System.Drawing.Size(204, 34);
+            this.htmlLabel2.AutoSize = false;
+            this.htmlLabel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.htmlLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.htmlLabel2.Location = new System.Drawing.Point(147, 165);
+            this.htmlLabel2.Name = "htmlLabel2";
+            this.htmlLabel2.Size = new System.Drawing.Size(209, 40);
+            this.htmlLabel2.TabIndex = 3;
+            this.htmlLabel2.Text = "Este es su Producto";
+            // 
+            // metroCheckBox1
+            // 
+            this.metroCheckBox1.AutoSize = true;
+            this.metroCheckBox1.Location = new System.Drawing.Point(147, 212);
+            this.metroCheckBox1.Name = "metroCheckBox1";
+            this.metroCheckBox1.Size = new System.Drawing.Size(32, 15);
+            this.metroCheckBox1.TabIndex = 4;
+            this.metroCheckBox1.Text = "SI";
+            this.metroCheckBox1.UseSelectable = true;
+            this.metroCheckBox1.CheckedChanged += new System.EventHandler(this.metroCheckBox1_CheckedChanged);
+            // 
+            // metroButton1Cotiza
+            // 
+            this.metroButton1Cotiza.Location = new System.Drawing.Point(209, 231);
+            this.metroButton1Cotiza.Name = "metroButton1Cotiza";
+            this.metroButton1Cotiza.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1Cotiza.TabIndex = 5;
+            this.metroButton1Cotiza.Text = "Resultado";
+            this.metroButton1Cotiza.UseSelectable = true;
+            this.metroButton1Cotiza.Click += new System.EventHandler(this.metroButton1Cotiza_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 357);
+            this.Controls.Add(this.metroButton1Cotiza);
+            this.Controls.Add(this.metroCheckBox1);
+            this.Controls.Add(this.htmlLabel2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.htmlLabel1);
+            this.Controls.Add(this.htmlLabel1Resultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel1Resultado;
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel2;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
+        private MetroFramework.Controls.MetroButton metroButton1Cotiza;
+    }
+}
+
---
+++ IVA/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IVA
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void htmlLabel1_Click(object sender, EventArgs e)
+        {
+        }
+        private void metroCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+        private void metroButton1Cotiza_Click(object sender, EventArgs e)
+        {
+            double precio = double.Parse(metroTextBox1.Text);
+            bool esMedicina = metroCheckBox1.Checked;
+            double IVA = 0.0;
+
+            if (!esMedicina) 
+            {
+                IVA = precio * 0.16;
+            }
+            double precioTotal = precio + IVA;
+
+            htmlLabel1Resultado.Text = $"El precio total de pagar es de: {precioTotal:C}";
+        }
+    }
+}
